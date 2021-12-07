dfd19495d424180b0985904ab36c149dc2782830
LONGXiaoyu661105 <secretary903@outlook.com>
2021-12-07 16:59:55 +0800

    [dev] Add advertisers in ctor

--- src/OctomapProcessing.cpp
+++ src/OctomapProcessing.cpp
@@ -19,9 +19,9 @@ OctomapProcessing::OctomapProcessing(float alpha, Vehicle &v,
     float maxRange, ros::NodeHandle n) :
   vehicle(v), maxRange(maxRange), gotGoal(false), gotOcto(false), alpha(alpha)
 {
-  histogram_pub = n.advertise<sensor_msgs::PointCloud2>("histogram", 2);
-  pose_pub = n.advertise<geometry_msgs::PoseArray>("open_poses", 2);
-  next_pose_pub = n.advertise<geometry_msgs::PoseStamped>("next_direction", 2);
+  histogram_pub = n.advertise<sensor_msgs::PointCloud2>("/histogram", 2);
+  pose_pub = n.advertise<geometry_msgs::PoseArray>("/open_poses", 2);
+  next_pose_pub = n.advertise<geometry_msgs::PoseStamped>("/next_direction", 2);
 }
 
 void OctomapProcessing::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
@@ -37,7 +37,6 @@ void OctomapProcessing::goalCallback(const geometry_msgs::PoseStamped::ConstPtr&
   }
 
 }
-
 
 void OctomapProcessing::octomapCallback(const octomap_msgs::Octomap::ConstPtr& msg) {
   octomap::AbstractOcTree* atree = octomap_msgs::msgToMap(*msg);
@@ -69,7 +68,7 @@ void OctomapProcessing::process() {
     exit(0);
   // Disp string
   // Disp histogram point cloud
-*/
+ */
   RGBPointCloud::Ptr pc = h.displayCloud(1);
   sensor_msgs::PointCloud2 pc_msg;
   pcl::toROSMsg(*pc, pc_msg);
