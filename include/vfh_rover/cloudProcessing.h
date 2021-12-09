4a2b80a8b496ae5cddb7e2f2c718fba570d22f45
LONGXiaoyu661105 <secretary903@outlook.com>
2021-12-09 16:11:11 +0800

    [dev] Modify Add headers, add cloud process to octomap

--- include/vfh_rover/cloudProcessing.h
+++ include/vfh_rover/cloudProcessing.h
@@ -1,15 +1,55 @@
+#pragma once
+#define PCL_NO_PRECOMPILE
+
 #include <cstddef>
 #include<cmath>
+#include <cmath>
+#include <mutex>
+#include <queue>
+#include <vector>
+#include <deque>
 
+#include <pcl/pcl_macros.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include "pcl/impl/point_types.hpp"
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/search/impl/kdtree.hpp>
+#include <pcl/registration/gicp.h>
+#include <pcl/registration/icp.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl/octree/octree_search.h>
-#include <pcl/octree/octree_pointcloud_occupancy.h>
 
-using namespace pcl::octree;
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+#include <eigen3/Eigen/Dense>
+
+#include <octomap/octomap.h>
+#include <octomap/math/Vector3.h>
+#include <octomap_msgs/Octomap.h>
+#include <octomap_msgs/conversions.h>
+
+using PointType = pcl::PointXYZI;
+using namespace octomap;
 
 //Unit in m.
 constexpr double RESOLUTION = 0.5;
-constexpr double HIST_RESOLUTION = 2.0;
-constexpr double DRONE_SIZE = 2.0;
-constexpr double SAFETY_DIST = 5.0;
-constexpr double BOX_LENGTH = 5.0;
+
+ros::NodeHandle nh;
+
+ros::Subscriber subCropLaserCloudMap;
+ros::Publisher pubOcto;
+
+OcTree tree (RESOLUTION);  // create empty tree with resolution 0.5
+
+pcl::PointCloud<PointType>::Ptr laserCloudCrop{new pcl::PointCloud<PointType>};
--- src/cloudProcessing.cpp
+++ src/cloudProcessing.cpp
@@ -1,173 +1,28 @@
 #include "include/vfh_rover/cloudProcessing.h"
 
-
-
-struct voxel_hist{
-    float azimuth;
-    float elevation;
-    float dist;
-    bool isReached;
-    voxel_hist(float a, float e):azimuth(a), elevation(e),dist(0.f){}
-};
-
-using PolarHist = voxel_hist*;
-class local_planner
+void laserCloudCropHandler(const sensor_msgs::PointCloud2ConstPtr &laserCloudCropRes)
 {
-private:
+    pcl::fromROSMsg(*laserCloudCropRes, *laserCloudCrop);
+}
 
-    ros::NodeHandle nh;
-
-    ros::Subscriber subOdomAftMapped, subCropLaserCloudMap;
-    ros::Publisher pubPathAvoidence, pubOctoGrid;
-
-    OctreePointCloudSearch<PointType>::Ptr octomap{new OctreePointCloudSearch<PointType>(RESOLUTION)};
-    OctreePointCloudOccupancy<PointType>::Ptr occupancy{new OctreePointCloudOccupancy<PointType>(RESOLUTION)};
-
-    std::vector<PolarHist> hist;
-
-    pcl::PointCloud<PointType>::Ptr laserCloudCrop{new pcl::PointCloud<PointType>};
-
-    double vcs_x = 0.0f, vcs_y = 0.0f, vcs_z = 0.0f;
-
-public:
-
-    local_planner(){
-        //Read raw pointcloud map
-        subOdomAftMapped = nh.subscribe<nav_msgs::Odometry>("/aft_mapped_to_init", 100, &local_planner::odomAftMappedHandler, this);
-        subCropLaserCloudMap = nh.subscribe<sensor_msgs::PointCloud2>("/laser_cloud_map_bounded", 100, &local_planner::laserCloudCropHandler, this);
+//Save input pcd into octree.
+void cloudProcess(const pcl::PointCloud<PointType>::Ptr& cloud_in)
+{
+    for(int i = 0; i < cloud_in->size(); i++){
+        point3d point(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
+        tree.updateNode(point, true);
     }
-    local_planner(double leaf_size_){
-        local_planner();
-        octomap->setResolution(leaf_size_);
-    }
-    ~local_planner() = default;
+}
 
-    void laserCloudCropHandler(const sensor_msgs::PointCloud2ConstPtr &laserCloudCropRes){
-        pcl::fromROSMsg(*laserCloudCropRes, *laserCloudCrop);
-    }
-
-    void odomAftMappedHandler(const nav_msgs::OdometryConstPtr &odomAftMappedRes){
-        vcs_x = odomAftMappedRes->pose.pose.position.x;
-        vcs_y = odomAftMappedRes->pose.pose.position.y;
-        vcs_z = odomAftMappedRes->pose.pose.position.z;
-    }
-
-    //Save input pcd into octree.
-    void set_cloud_octomap(const pcl::PointCloud<PointType>::Ptr& cloud_in)
-    {
-        octomap->setInputCloud(cloud_in);
-        octomap->addPointsFromInputCloud();
-    }
-
-    void set_vcs(const Eigen::Vector3f& pos){
-        vcs_x = pos[0]; vcs_y = pos[1]; vcs_z = pos[2];
-    }
-    //Update pointcloud in octomap.
-    void update_cloud_octomap(const pcl::PointCloud<PointType>::Ptr& update_cloud)
-    {
-        set_cloud_octomap(update_cloud);
-    }
-
-    //Return a result pointcloud ptr. 
-    pcl::PointCloud<PointType>::Ptr octreeRadiusSearch(float search_radius){
-
-        std::vector<int>pointIdxRadiusSearch;
-        std::vector<float>pointRadiusSquaredDist;
-
-        PointType searchPoint;
-        pcl::PointCloud<PointType> cloud_box = *octomap->getInputCloud();
-        pcl::PointCloud<PointType> cloud_sphere;
-
-        if(octomap->radiusSearch(searchPoint, (double)search_radius,pointIdxRadiusSearch, pointRadiusSquaredDist) > 0){
-            for(std::size_t i = 0; i < pointIdxRadiusSearch.size(); i++){
-                cloud_sphere.push_back(cloud_box[i]);
-            }
-            std::cout << "[local_planner]: Radius search completed!" << std::endl;
-        }
-
-        return cloud_sphere.makeShared();
-    }
-
-    void printVCS(){
-        ROS_INFO("[local planner]: vcs_x(%.2f m), vcs_y(%.2f m), vcs_z(%.2f m)", vcs_x, vcs_y, vcs_z);
-    }
-
-    float calculateAzimuth(float x_i, float y_i){
-        return floor(atan(x_i-vcs_x/y_i-vcs_y)/HIST_RESOLUTION);
-    }
-
-    float calculateElevation(float x_i, float y_i, float z_i){
-        return floor(atan(z_i-vcs_z/calNorm2(x_i-vcs_x, y_i-vcs_y))/HIST_RESOLUTION);
-    }
-
-    float calculateDist(float x_i, float y_i, float z_i){
-        return floor(calNorm2(x_i-vcs_x, y_i-vcs_y, z_i-vcs_z));
-    }
-    
-    void buildOccupancy(){
-        occupancy->setInputCloud(octomap->getInputCloud());
-    }
-
-    void buildPolarHistgram(){
-        //Note: Read VCS from topic:aftmapped_odom.
-        pcl::PointCloud<PointType> temp = *octomap->getInputCloud();
-        int cloudSize  = temp.size();
-
-        for(int i = 0; i < cloudSize; i++){
-            PolarHist i_hist = new voxel_hist(calculateAzimuth(temp[i].x, temp[i].y), calculateElevation(temp[i].x, temp[i].y,temp[i].z));
-            i_hist->dist = calculateDist(temp[i].x, temp[i].y,temp[i].z);
-             hist.push_back(i_hist);
-        }
-
-    }
-
-    void addAuxiliaryInfo(){
-
-    }
-
-    void processPointcloud(const pcl::PointCloud<PointType>::Ptr& laser_map_bounded){
-        set_cloud_octomap(laser_map_bounded);
-        update_cloud_octomap(octreeRadiusSearch(BOX_LENGTH/2));
-    }
-
-    void process(){
-
-        //Stage.1: Get octomap from raw pointcloud.
-        processPointcloud(laserCloudCrop);
-
-        //Stage.2: Build 2D histogram.
-        buildPolarHistgram();
-        buildOccupancy();
-
-        //Stage.3: Add new characteristics.
-        addAuxiliaryInfo();
-
-        //Stage.4: 2D Binary Histogram
-
-
-        //Stage.5:Path searching and selection.
-        
-
-        
-    }
-
-
-
-};
-
-//Debug:
 int main(int argc, char** argv){
-    ros::init(argc, argv, "localPlanner");
-    local_planner test;
-    ros::Rate rate(100);
+    ros::init(argc, argv, "cloudProcessNode");
 
-    ROS_INFO("[localPlanner]: Starting local planner.");
+    ROS_INFO("[cloudProcessNode]: Starting cloud process.");
 
-    // while(ros::ok()){
-    //     test.process();
-    //     ros::spinOnce();
-    //     rate.sleep();
-    // }
+    subCropLaserCloudMap = nh.subscribe<sensor_msgs::PointCloud2>("/laser_cloud_map_bounded", 100, laserCloudCropHandler);
 
+    pubOcto = nh.advertise<octomap_msgs::Octomap>("octomapCropped", 100);
+    
+    ros::spin();
     return 0;
 }
